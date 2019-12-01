c0e2efff9982d9c8994abdf97c2c45238feb271f
Ronan Pozzi <contact@treenity-web.fr>
2019-12-01 14:18:51 +0100

    Change autoinstall system

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,19 +11,19 @@ fi
 if [[ ! -f "/var/www/html/dev.json" ]];then
     echo ">>> Installing OroCommerce from https://github.com/oroinc/orocommerce-application.git"
     git clone https://github.com/oroinc/orocommerce-application.git /var/www/html
-    cd /var/www/html && composer install
 
     if [[ -d "/var/www/html/var/cache" ]];then
         echo ">>> cleaning /var/www/html/var/cache"
         rm -rf /var/www/html/var/cache
     fi
 
-    echo ">>> Running oro:install with admin user : johndoe@example.com/admin1234"
-    php /var/www/html/bin/console --env=prod oro:install --no-interaction --timeout 3600 --drop-database --user-name=admin --user-firstname=John --user-lastname=Doe --user-password=admin1234 --user-email=johndoe@example.com --organization-name=Acme --application-url=http://localhost/
-    echo ">>> Running console dumps"
-    php /var/www/html/bin/console --env=prod fos:js-routing:dump && php bin/console --env=prod oro:localization:dump && php bin/console --env=prod oro:assets:install && php bin/console --env=prod oro:translation:dump && php bin/console --env=prod oro:requirejs:build && php bin/console --env=prod assetic:dump
-    echo ">>> OroCommerce installed"
-    echo ">>> OroCommerce should be running now"
+    echo "run :"
+    echo "docker exec -it oro_webserver bash"
+    echo "then :"
+    echo "composer install"
+    echo "You should be asked for parameters.yml settup"
+    echo "php /var/www/html/bin/console --env=prod oro:install --no-interaction --timeout 3600 --drop-database --user-name=admin --user-firstname=John --user-lastname=Doe --user-password=admin1234 --user-email=johndoe@example.com --organization-name=Acme --application-url=http://localhost/"
+    echo "php /var/www/html/bin/console --env=prod fos:js-routing:dump && php bin/console --env=prod oro:localization:dump && php bin/console --env=prod oro:assets:install && php bin/console --env=prod oro:translation:dump && php bin/console --env=prod oro:requirejs:build && php bin/console --env=prod assetic:dump"
 fi
 
 # Enable xdebug
